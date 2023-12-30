e7a6785040694b0bc089f13bfa408e5dc92fcfa8
Charles McAnany <git@charlesmcanany.com>
2023-12-29 22:58:54 -0600

    Copyright notices added

--- src/libPdbToJson.d
+++ src/libPdbToJson.d
@@ -136,4 +136,10 @@ void runToJson(string pdbFname, char chain, bool allBackbone, char dynamicsAxis,
     auto outFp = File(outJson, "w");
     outFp.writeln(json.toString);
     outFp.close();
-}                
+}
+
+/*     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
